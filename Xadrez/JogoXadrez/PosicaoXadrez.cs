493cc5c950402c03294125733e67dbd3ed2b11de
Pedro Andrade <pfandrade10@hotmail.com>
2019-04-13 16:51:55 -0300

    Criada a classe (PosicaoXadrez)

---
+++ Xadrez/JogoXadrez/PosicaoXadrez.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace JogoXadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+           return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,20 +8,8 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.ColocarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 3));
-                tab.ColocarPeca(new Rei(Cor.Preto, tab), new Posicao(2, 4));
-
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
         }
     }
 }
