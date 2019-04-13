9bda167c2bf7845ae37147d87b7afa2615fd4a0e
Pedro Andrade <pfandrade10@hotmail.com>
2019-04-13 17:11:54 -0300

    Melhora do display com a adiçao de cor para peças pretas

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,8 +8,24 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.ColocarPeca(new Torre(Cor.Preto, tab), new Posicao(0,0));
+                tab.ColocarPeca(new Torre(Cor.Preto, tab), new Posicao(1,3));
+                tab.ColocarPeca(new Torre(Cor.Preto, tab), new Posicao(0,2));
+                tab.ColocarPeca(new Torre(Cor.Branco, tab), new Posicao(0, 7));
+                tab.ColocarPeca(new Torre(Cor.Branco, tab), new Posicao(4, 2));
+                tab.ColocarPeca(new Torre(Cor.Branco, tab), new Posicao(5, 3));
+
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -11,18 +11,36 @@ namespace Xadrez
         {
             for ( int i=0; i < Tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < Tab.Colunas; j++)
-                {
+                {                
                     if (Tab.Peca(i,j) is null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(Tab.Peca(i, j) + " ");
+                        ImprimirPeca(Tab.Peca(i, j));
+                        Console.Write(" ");
                     }                    
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if  (peca.Cor == Cor.Branco)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
