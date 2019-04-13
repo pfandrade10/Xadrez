36c30fcd32087849bb9cd4594afa621d0d8ae3c3
Pedro Andrade <pfandrade10@hotmail.com>
2019-04-13 12:05:02 -0300

    Projeto inicial Xadrez

---
+++ Xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
